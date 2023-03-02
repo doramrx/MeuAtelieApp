1e73fd7edd90d6cf25b92994e0a1889c79b21045
Isadora <isah3027@gmail.com>
2023-03-01 21:37:53 -0300

    feat: Create Home Screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <SignIn />
+
+    // <SignIn/>
+
+    <Home/>
+
   );
 }
