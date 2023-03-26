789e518a6e74f6bca763cbdad87a3c5de56369ac
IgorIA <igorsemphoski@gmail.com>
2023-03-26 15:29:16 -0300

    feat: add swipeable effect to dressmakers items

--- mobile/src/screens/ListDressMakers/index.tsx
+++ mobile/src/screens/ListDressMakers/index.tsx
@@ -1,10 +1,16 @@
-import { DrawerScreenProps } from "@react-navigation/drawer";
-import { ParamListBase, useFocusEffect } from "@react-navigation/native";
+import { useCallback, useState } from "react";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 
+import { DrawerScreenProps } from "@react-navigation/drawer";
+import { ParamListBase, useFocusEffect } from "@react-navigation/native";
+
+import { Swipeable } from "react-native-gesture-handler";
+
+import Icon from "react-native-vector-icons/FontAwesome5";
+
 import { styles } from "./styles";
+
 import HamburguerIcon from "../../assets/Hamburguer_icon.png";
-import { useCallback, useState } from "react";
 import { database } from "../../database/database";
 
 interface Dressmaker {
@@ -69,20 +75,33 @@ export function ListDressMakers({
                 data={dressmakers}
                 renderItem={({ item }) => {
                     return (
+                        <DressMakerItem
+                            id={item.id}
+                            name={item.name}
+                            phoneNumber={item.phoneNumber}
+                        />
+                    );
+                }}
+                ItemSeparatorComponent={() => {
+                    return (
                         <View
-                            key={item.id}
-                            style={styles.dressMakerBlock}
-                        >
-                            <View style={styles.dressMakerAvatar}></View>
-                            <View>
-                                <Text style={styles.dressMakerName}>
-                                    {item.name}
-                                </Text>
-                                <Text style={styles.dressMakerPhoneNumber}>
-                                    Telefone: {item.phoneNumber}
-                                </Text>
-                            </View>
-                        </View>
+                            style={{
+                                height: 1,
+                                width: "100%",
+                                backgroundColor: "#D9D9D9",
+                            }}
+                        />
+                    );
+                }}
+                ListFooterComponent={() => {
+                    return (
+                        <View
+                            style={{
+                                height: 1,
+                                width: "100%",
+                                backgroundColor: "#D9D9D9",
+                            }}
+                        />
                     );
                 }}
             />
@@ -95,6 +114,59 @@ export function ListDressMakers({
                     <Text style={styles.footerAddButtonText}>+</Text>
                 </TouchableOpacity>
             </View>
+        </View>
+    );
+}
+
+function DressMakerItem({ id, name, phoneNumber }: Dressmaker) {
+    return (
+        <Swipeable
+            onSwipeableOpen={(direction) => {
+                if (direction === "right") {
+                    return;
+                }
+            }}
+            renderLeftActions={LeftItem}
+            overshootLeft={false}
+        >
+            <View
+                key={id}
+                style={styles.dressMakerBlock}
+            >
+                <View style={styles.dressMakerAvatar}></View>
+                <View>
+                    <Text style={styles.dressMakerName}>{name}</Text>
+                    <Text style={styles.dressMakerPhoneNumber}>
+                        Telefone: {phoneNumber}
+                    </Text>
+                </View>
+            </View>
+        </Swipeable>
+    );
+}
+
+function LeftItem() {
+    return (
+        <View style={styles.actionButtonsContainer}>
+            <TouchableOpacity
+                style={[styles.trashButton, styles.actionButtons]}
+            >
+                <Icon
+                    name="trash"
+                    size={15}
+                    color="#FFF"
+                />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={[styles.editButton, styles.actionButtons]}>
+                <Text>
+                    <Icon
+                        name="pen"
+                        size={15}
+                        color="#FFF"
+                    />
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
