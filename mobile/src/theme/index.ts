f792504ecfb87aa5a9de2459a6b208e594a51711
IgorIA <igorsemphoski@gmail.com>
2023-04-21 20:37:13 -0300

    feat(css): map used colors into a javascript object

---
+++ mobile/src/theme/index.ts
@@ -0,0 +1,30 @@
+export const THEME = {
+    COLORS: {
+        RED: "#E7232C",
+        WHITE: "#FFF",
+        BLACK: "#000",
+        BLUE: "#34DDF4",
+        GREEN: "#23F979",
+
+        PINK: {
+            V1: "#FB6871",
+            V2: "#FF9FA4",
+        },
+
+        GRAY: {
+            DARK: {
+                V1: "#313131",
+                V2: "#484848",
+                V3: "#5C5C5C",
+            },
+            MEDIUM: {
+                V1: "#8C8C8C",
+                V2: "#A6A6A6",
+            },
+            LIGH: {
+                V1: "#CECECE",
+                V2: "#F4F4F4",
+            },
+        },
+    },
+};
