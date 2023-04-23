c25db51aa461abff09ccc8d9bcca672d4e40babb
IgorIA <igorsemphoski@gmail.com>
2023-04-22 23:37:09 -0300

    improve(css): redesign login page layout

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -6,3 +6,20 @@ defaultConfig.resolver.assetExts.push("db");
 
 module.exports = defaultConfig;
 module.exports = getDefaultConfig(__dirname);
+module.exports = (() => {
+    const config = getDefaultConfig(__dirname);
+
+    const { transformer, resolver } = config;
+
+    config.transformer = {
+        ...transformer,
+        babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    };
+    config.resolver = {
+        ...resolver,
+        assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+        sourceExts: [...resolver.sourceExts, "svg"],
+    };
+
+    return config;
+})();
