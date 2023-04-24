5c03c64026e42e922654d4d7dea988852d6091f3
IgorIA <igorsemphoski@gmail.com>
2023-04-24 20:57:35 -0300

    feat: create custom modal component

---
+++ mobile/src/components/shared/CustomModal/index.tsx
@@ -0,0 +1,107 @@
+import { ReactNode, useState } from "react";
+import { Text, View, Modal, TouchableHighlight } from "react-native";
+
+import { styles } from "./styles";
+import { THEME } from "../../../theme";
+
+interface Props {
+    modalTitle: string;
+    modalColor: string;
+    modalIcon: ReactNode;
+    closeModalText: string;
+    closeModalButtonStyle?: "filled" | "transparent";
+    actionButtonText?: string;
+    children?: ReactNode;
+}
+
+export function CustomModal({
+    modalTitle,
+    modalIcon,
+    modalColor,
+    actionButtonText,
+    closeModalText,
+    closeModalButtonStyle = "transparent",
+    children,
+}: Props) {
+    const [isOpen, setIsOpen] = useState(true);
+
+    return (
+        <Modal
+            animationType="fade"
+            transparent={true}
+            visible={isOpen}
+        >
+            <View style={styles.wrapper}>
+                <View style={styles.container}>
+                    <View
+                        style={[styles.header, { backgroundColor: modalColor }]}
+                    >
+                        {modalIcon}
+                    </View>
+
+                    <View style={styles.main}>
+                        <Text style={styles.title}>{modalTitle}</Text>
+
+                        {children}
+
+                        <View style={styles.buttonsContainer}>
+                            <TouchableHighlight
+                                underlayColor={
+                                    closeModalButtonStyle === "transparent"
+                                        ? THEME.COLORS.GRAY.LIGH.V2
+                                        : modalColor
+                                }
+                                activeOpacity={0.5}
+                                onPress={() => {
+                                    setIsOpen(false);
+                                }}
+                                style={[
+                                    styles.button,
+                                    actionButtonText !== undefined && {
+                                        marginRight: 10,
+                                    },
+                                    styles.closeModalButton,
+                                    closeModalButtonStyle === "filled" && {
+                                        borderWidth: 0,
+                                        backgroundColor: modalColor,
+                                    },
+                                    actionButtonText === undefined && {
+                                        flex: 0,
+                                        width: 130,
+                                    },
+                                ]}
+                            >
+                                <Text
+                                    style={[
+                                        styles.buttonText,
+                                        styles.closeModalText,
+                                        closeModalButtonStyle === "filled" && {
+                                            color: THEME.COLORS.WHITE,
+                                        },
+                                    ]}
+                                >
+                                    {closeModalText}
+                                </Text>
+                            </TouchableHighlight>
+                            {actionButtonText && (
+                                <TouchableHighlight
+                                    underlayColor={modalColor}
+                                    activeOpacity={0.5}
+                                    onPress={() => {}}
+                                    style={[
+                                        styles.button,
+                                        { backgroundColor: modalColor },
+                                    ]}
+                                >
+                                    <Text style={[styles.buttonText]}>
+                                        {actionButtonText}
+                                    </Text>
+                                </TouchableHighlight>
+                            )}
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+}
---
+++ mobile/src/components/shared/CustomModal/styles.ts
@@ -0,0 +1,59 @@
+import { Dimensions, StyleSheet } from "react-native";
+import { THEME } from "../../../theme";
+
+export const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(0,0,0,0.22)",
+    },
+    container: {
+        width: Dimensions.get("screen").width * 0.7,
+    },
+    header: {
+        alignItems: "center",
+        justifyContent: "center",
+        height: 90,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    main: {
+        backgroundColor: THEME.COLORS.WHITE,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        paddingBottom: 30,
+        paddingTop: 15,
+        paddingHorizontal: 20,
+    },
+    title: {
+        textAlign: "center",
+        fontWeight: THEME.FONT.WEIGHT.MEDIUM as any,
+        fontSize: 22,
+        marginBottom: 16
+    },
+    buttonsContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 16,
+    },
+    button: {
+        flex: 1,
+        borderRadius: 12,
+        paddingVertical: 8,
+    },
+    closeModalButton: {
+        borderWidth: 1,
+        borderColor: THEME.COLORS.GRAY.LIGH.V1,
+    },
+    closeModalText: {
+        color: THEME.COLORS.GRAY.MEDIUM.V1,
+    },
+    buttonText: {
+        textAlign: "center",
+        fontWeight: THEME.FONT.WEIGHT.MEDIUM as any,
+        fontSize: 16,
+        color: THEME.COLORS.WHITE
+    },
+});
