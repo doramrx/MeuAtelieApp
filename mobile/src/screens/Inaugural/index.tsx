5525108361eef52148ea4af8e79e526d2bc8231b
IgorIA <igorsemphoski@gmail.com>
2023-07-08 20:23:11 -0300

    style: change identation size

--- mobile/src/screens/Inaugural/index.tsx
+++ mobile/src/screens/Inaugural/index.tsx
@@ -8,49 +8,49 @@ import Logo from "../../assets/Logo.png";
 import { Screen } from "../../components/shared/Screen";
 
 export function Inaugural() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    function navigateTo(path: "signIn" | "signUp") {
-        navigation.navigate(path);
-    }
+  function navigateTo(path: "signIn" | "signUp") {
+    navigation.navigate(path);
+  }
 
-    return (
-        <Screen.Root>
-            <Screen.Header additionalStyles={styles.logoContainer}>
-                <Image
-                    source={Logo}
-                    style={styles.logoImage}
-                />
-            </Screen.Header>
+  return (
+    <Screen.Root>
+      <Screen.Header additionalStyles={styles.logoContainer}>
+        <Image
+          source={Logo}
+          style={styles.logoImage}
+        />
+      </Screen.Header>
 
-            <Screen.Content additionalStyles={styles.welcomeContainer}>
-                <Text style={styles.title}>Bem vindo (a)</Text>
-                <Text style={styles.message}>
-                    O melhor aplicativo para organizar o seu Ateliê!
-                </Text>
+      <Screen.Content additionalStyles={styles.welcomeContainer}>
+        <Text style={styles.title}>Bem vindo (a)</Text>
+        <Text style={styles.message}>
+          O melhor aplicativo para organizar o seu Ateliê!
+        </Text>
 
-                <TouchableHighlight
-                    style={[styles.button, styles.firstButton]}
-                    underlayColor={THEME.COLORS.PINK.V2_UNDERLAY}
-                    activeOpacity={0.98}
-                    onPress={() => {
-                        navigateTo("signIn");
-                    }}
-                >
-                    <Text style={styles.buttonText}>Entrar</Text>
-                </TouchableHighlight>
+        <TouchableHighlight
+          style={[styles.button, styles.firstButton]}
+          underlayColor={THEME.COLORS.PINK.V2_UNDERLAY}
+          activeOpacity={0.98}
+          onPress={() => {
+            navigateTo("signIn");
+          }}
+        >
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableHighlight>
 
-                <TouchableHighlight
-                    style={styles.button}
-                    underlayColor={THEME.COLORS.PINK.V2_UNDERLAY}
-                    activeOpacity={0.98}
-                    onPress={() => {
-                        navigateTo("signUp");
-                    }}
-                >
-                    <Text style={styles.buttonText}>Cadastrar</Text>
-                </TouchableHighlight>
-            </Screen.Content>
-        </Screen.Root>
-    );
+        <TouchableHighlight
+          style={styles.button}
+          underlayColor={THEME.COLORS.PINK.V2_UNDERLAY}
+          activeOpacity={0.98}
+          onPress={() => {
+            navigateTo("signUp");
+          }}
+        >
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        </TouchableHighlight>
+      </Screen.Content>
+    </Screen.Root>
+  );
 }
--- mobile/src/screens/Inaugural/styles.ts
+++ mobile/src/screens/Inaugural/styles.ts
@@ -4,49 +4,49 @@ import { THEME } from "../../theme";
 const pixelDensity = PixelRatio.get();
 
 export const styles = StyleSheet.create({
-    logoContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  logoContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    logoImage: {
-        width: (400 * 1) / pixelDensity,
-        height: (400 * 1) / pixelDensity,
-    },
+  logoImage: {
+    width: (400 * 1) / pixelDensity,
+    height: (400 * 1) / pixelDensity,
+  },
 
-    welcomeContainer: {
-        flex: 1.5,
-        paddingTop: 40,
-        paddingHorizontal: 40,
-    },
+  welcomeContainer: {
+    flex: 1.5,
+    paddingTop: 40,
+    paddingHorizontal: 40,
+  },
 
-    title: {
-        fontSize: 28,
-        fontWeight: "600",
-        marginBottom: 50,
-    },
-    message: {
-        fontSize: 22,
-        fontWeight: "300",
-        marginBottom: 80,
-    },
+  title: {
+    fontSize: 28,
+    fontWeight: "600",
+    marginBottom: 50,
+  },
+  message: {
+    fontSize: 22,
+    fontWeight: "300",
+    marginBottom: 80,
+  },
 
-    firstButton: {
-        marginBottom: 18,
-    },
+  firstButton: {
+    marginBottom: 18,
+  },
 
-    button: {
-        backgroundColor: THEME.COLORS.PINK.V2,
-        paddingVertical: 10,
-        borderRadius: 12,
-        elevation: 4,
-        shadowColor: THEME.COLORS.BLACK,
-    },
+  button: {
+    backgroundColor: THEME.COLORS.PINK.V2,
+    paddingVertical: 10,
+    borderRadius: 12,
+    elevation: 4,
+    shadowColor: THEME.COLORS.BLACK,
+  },
 
-    buttonText: {
-        color: THEME.COLORS.WHITE.FULL_WHITE,
-        textAlign: "center",
-        fontSize: 20,
-    },
+  buttonText: {
+    color: THEME.COLORS.WHITE.FULL_WHITE,
+    textAlign: "center",
+    fontSize: 20,
+  },
 });
